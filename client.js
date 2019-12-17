9c92daced0543396b34b10121fff847fd913c3a0
aprots <aprots@hubspot.com>
2019-12-17 11:07:43 +0200

    Add support of several client instances
    Now it should be possible to have several client instances with own set of options

--- client.js
+++ client.js
@@ -7,185 +7,185 @@ const qs = require('querystring')
 
 const DEFAULT_HEADERS = { 'User-Agent': `${pjson.name}_${pjson.version}` }
 const API_TIMEOUT = 60000
-const objectsApiClient = new objectClientFactory.ApiClient()
-const pipelinesApiClient = new pipelinesClientFactory.ApiClient()
-
-const API_CLIENTS = [objectsApiClient, pipelinesApiClient];
-
-let accessToken
-let clientId
-let clientSecret
-let redirectUri
-let refreshToken
-let apiTimeout
-let basePath
-
-const setPropertyForApiClients = (path, value) => {
-  const setFunction = _.partialRight(_.set, path, value)
-  _.map(API_CLIENTS, setFunction)
-}
-
-const setApiKey = (apiKey) => {
-  setPropertyForApiClients('authentications.hapikey.apiKey', apiKey)
-}
-
-const setTimeout = (timeout) => {
-  apiTimeout = timeout || API_TIMEOUT
-  setPropertyForApiClients('timeout', apiTimeout)
-}
-
-const setBasePath = (basePathToSet) => {
-  basePath = basePathToSet || 'https://api.hubapi.com'
-  objectsApiClient.basePath = `${basePath}/crm/v3/objects`.replace(/\/+$/, '')
-  pipelinesApiClient.basePath = `${basePath}crm/v3/pipelines`.replace(/\/+$/, '')
-}
-
-const setCache = (cache) => {
-  if (!_.isNil(cache)) {
-    setPropertyForApiClients('cache', cache)
-  }
-}
-
-const setEnableCookies = (enableCookies) => {
-  if (!_.isNil(enableCookies)) {
-    setPropertyForApiClients('enableCookies', enableCookies)
-  }
-}
-
-const setAgent = (agent) => {
-  if (!_.isNil(agent)) {
-    setPropertyForApiClients('agent', agent)
-  }
-}
-
-const setRequestAgent = (requestAgent) => {
-  if (!_.isNil(requestAgent)) {
-    setPropertyForApiClients('requestAgent', requestAgent)
-  }
-}
-
-const setAccessToken = (accessTokenToSet) => {
-  accessToken = accessTokenToSet
-  setPropertyForApiClients('authentications.accessToken', accessToken)
-}
-
-const setDefaultHeaders = (defaultHeadersToSet) => {
-  const defaultHeaders = _.assign({}, defaultHeadersToSet, DEFAULT_HEADERS)
-  setPropertyForApiClients('defaultHeaders', defaultHeaders)
-}
-
-const setAuth = (options = {}) => {
-  if (options.apiKey) {
-    setApiKey(options.apiKey)
-  } else if (options.accessToken) {
-    setAccessToken(options.accessToken)
-  }
-}
-
-const setOAuth = (options = {}) => {
-  clientId = options.clientId
-  clientSecret = options.clientSecret
-  redirectUri = options.redirectUri
-  refreshToken = options.refreshToken
-}
-
-const getOptions = () => {
-  return {
-    timeout: apiTimeout,
-    basePath,
-    defaultHeaders: objectsApiClient.defaultHeaders,
-    cache: objectsApiClient.cache,
-    enableCookies: objectsApiClient.enableCookies,
-  }
-}
-
-const apiRequest = (opts) => {
-  const params = _.cloneDeep(opts)
-  if (this.auth) {
-    params.auth = this.auth
-  }
-  params.method = params.method || 'GET'
-  params.json = true
-  params.resolveWithFullResponse = true
-
-  params.url = params.overlapUrl || basePath + (params.path || '')
-  delete params.overlapUrl
-  delete params.path
-  params.qs = Object.assign({}, this.qs, params.qs)
-
-  params.qsStringifyOptions = {
-    arrayFormat: 'repeat',
-  }
-
-  params.timeout = apiTimeout
-
-  return request(params).then((res) => res.body)
-}
-
-const getAuthorizationUrl = (newData) => {
-  const initialParams = {}
-  if (clientId) initialParams.client_id = clientId
-  if (redirectUri) initialParams.redirect_uri = redirectUri
-
-  const params = Object.assign({}, initialParams, newData)
-  return `https://app.hubspot.com/oauth/authorize?${qs.stringify(params)}`
-}
-
-const getAccessToken = (newData) => {
-  const initialData = {
-    grant_type: 'authorization_code',
-    client_id: clientId,
-    client_secret: clientSecret,
-    redirect_uri: redirectUri,
-  }
-  const form = Object.assign({}, initialData, newData)
-  return apiRequest({
-    method: 'POST',
-    path: '/oauth/v1/token',
-    form,
-  })
-}
-
-const refreshAccessToken = (newData) => {
-  const initialData = {
-    grant_type: 'refresh_token',
-    client_id: clientId,
-    client_secret: clientSecret,
-    redirect_uri: redirectUri,
-    refresh_token: refreshToken,
-  }
-  const form = Object.assign({}, initialData, newData)
-
-  return apiRequest({
-    method: 'POST',
-    path: '/oauth/v1/token',
-    form,
-  }).then((results) => {
-    setAccessToken(results.access_token) // refresh the new access_token on the client
-    return results
-  })
-}
-
-const getPortalInfo = () => {
-  return apiRequest({
-    method: 'GET',
-    path: `/oauth/v1/access-tokens/${accessToken}`,
-  })
-}
-
-const setOptions = (options) => {
-  setAuth(options)
-  setOAuth(options)
-  setTimeout(options.timeout)
-  setBasePath(options.basePath)
-  setDefaultHeaders(options.defaultHeaders)
-  setCache(options.cache)
-  setEnableCookies(options.enableCookies)
-  setAgent(options.agent)
-  setRequestAgent(options.requestAgent)
-}
 
 module.exports = (opts = {}) => {
+  const objectsApiClient = new objectClientFactory.ApiClient()
+  const pipelinesApiClient = new pipelinesClientFactory.ApiClient()
+
+  const API_CLIENTS = [objectsApiClient, pipelinesApiClient];
+
+  let accessToken
+  let clientId
+  let clientSecret
+  let redirectUri
+  let refreshToken
+  let apiTimeout
+  let basePath
+
+  const setPropertyForApiClients = (path, value) => {
+    const setFunction = _.partialRight(_.set, path, value)
+    _.map(API_CLIENTS, setFunction)
+  }
+
+  const setApiKey = (apiKey) => {
+    setPropertyForApiClients('authentications.hapikey.apiKey', apiKey)
+  }
+
+  const setTimeout = (timeout) => {
+    apiTimeout = timeout || API_TIMEOUT
+    setPropertyForApiClients('timeout', apiTimeout)
+  }
+
+  const setBasePath = (basePathToSet) => {
+    basePath = basePathToSet || 'https://api.hubapi.com'
+    objectsApiClient.basePath = `${basePath}/crm/v3/objects`.replace(/\/+$/, '')
+    pipelinesApiClient.basePath = `${basePath}crm/v3/pipelines`.replace(/\/+$/, '')
+  }
+
+  const setCache = (cache) => {
+    if (!_.isNil(cache)) {
+      setPropertyForApiClients('cache', cache)
+    }
+  }
+
+  const setEnableCookies = (enableCookies) => {
+    if (!_.isNil(enableCookies)) {
+      setPropertyForApiClients('enableCookies', enableCookies)
+    }
+  }
+
+  const setAgent = (agent) => {
+    if (!_.isNil(agent)) {
+      setPropertyForApiClients('agent', agent)
+    }
+  }
+
+  const setRequestAgent = (requestAgent) => {
+    if (!_.isNil(requestAgent)) {
+      setPropertyForApiClients('requestAgent', requestAgent)
+    }
+  }
+
+  const setAccessToken = (accessTokenToSet) => {
+    accessToken = accessTokenToSet
+    setPropertyForApiClients('authentications.accessToken', accessToken)
+  }
+
+  const setDefaultHeaders = (defaultHeadersToSet) => {
+    const defaultHeaders = _.assign({}, defaultHeadersToSet, DEFAULT_HEADERS)
+    setPropertyForApiClients('defaultHeaders', defaultHeaders)
+  }
+
+  const setAuth = (options = {}) => {
+    if (options.apiKey) {
+      setApiKey(options.apiKey)
+    } else if (options.accessToken) {
+      setAccessToken(options.accessToken)
+    }
+  }
+
+  const setOAuth = (options = {}) => {
+    clientId = options.clientId
+    clientSecret = options.clientSecret
+    redirectUri = options.redirectUri
+    refreshToken = options.refreshToken
+  }
+
+  const getOptions = () => {
+    return {
+      timeout: apiTimeout,
+      basePath,
+      defaultHeaders: objectsApiClient.defaultHeaders,
+      cache: objectsApiClient.cache,
+      enableCookies: objectsApiClient.enableCookies,
+    }
+  }
+
+  const apiRequest = (opts) => {
+    const params = _.cloneDeep(opts)
+    if (this.auth) {
+      params.auth = this.auth
+    }
+    params.method = params.method || 'GET'
+    params.json = true
+    params.resolveWithFullResponse = true
+
+    params.url = params.overlapUrl || basePath + (params.path || '')
+    delete params.overlapUrl
+    delete params.path
+    params.qs = Object.assign({}, this.qs, params.qs)
+
+    params.qsStringifyOptions = {
+      arrayFormat: 'repeat',
+    }
+
+    params.timeout = apiTimeout
+
+    return request(params).then((res) => res.body)
+  }
+
+  const getAuthorizationUrl = (newData) => {
+    const initialParams = {}
+    if (clientId) initialParams.client_id = clientId
+    if (redirectUri) initialParams.redirect_uri = redirectUri
+
+    const params = Object.assign({}, initialParams, newData)
+    return `https://app.hubspot.com/oauth/authorize?${qs.stringify(params)}`
+  }
+
+  const getAccessToken = (newData) => {
+    const initialData = {
+      grant_type: 'authorization_code',
+      client_id: clientId,
+      client_secret: clientSecret,
+      redirect_uri: redirectUri,
+    }
+    const form = Object.assign({}, initialData, newData)
+    return apiRequest({
+      method: 'POST',
+      path: '/oauth/v1/token',
+      form,
+    })
+  }
+
+  const refreshAccessToken = (newData) => {
+    const initialData = {
+      grant_type: 'refresh_token',
+      client_id: clientId,
+      client_secret: clientSecret,
+      redirect_uri: redirectUri,
+      refresh_token: refreshToken,
+    }
+    const form = Object.assign({}, initialData, newData)
+
+    return apiRequest({
+      method: 'POST',
+      path: '/oauth/v1/token',
+      form,
+    }).then((results) => {
+      setAccessToken(results.access_token) // refresh the new access_token on the client
+      return results
+    })
+  }
+
+  const getPortalInfo = () => {
+    return apiRequest({
+      method: 'GET',
+      path: `/oauth/v1/access-tokens/${accessToken}`,
+    })
+  }
+
+  const setOptions = (options) => {
+    setAuth(options)
+    setOAuth(options)
+    setTimeout(options.timeout)
+    setBasePath(options.basePath)
+    setDefaultHeaders(options.defaultHeaders)
+    setCache(options.cache)
+    setEnableCookies(options.enableCookies)
+    setAgent(options.agent)
+    setRequestAgent(options.requestAgent)
+  }
   setOptions(opts)
   const basicClient = new objectClientFactory.BasicApi(objectsApiClient)
   const batchClient = new objectClientFactory.BatchApi(objectsApiClient)
