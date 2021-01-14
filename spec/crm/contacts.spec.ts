7537d11bf7a40c2a8c7336c11cdc4ef6f16c3301
Aleh Tanasiuk <atanasiuk@hubspot.com>
2021-01-14 11:43:33 +0300

    Add test github action

--- spec/crm/contacts.spec.ts
+++ spec/crm/contacts.spec.ts
@@ -1,10 +1,11 @@
-import 'jasmine'
-import * as apis from '../../codegen/crm/contacts/api'
 import { Client } from '../../index'
 
 describe('api client', () => {
-  it('discoverable', () => {
-    const client = new Client()
-    expect(client.crm.contacts.hasOwnProperty('basicApi')).toBeTruthy()
+  it('is discoverable', () => {
+    const client = new Client().crm.contacts
+    expect(client.hasOwnProperty('basicApi')).toBeTruthy()
+    expect(client.hasOwnProperty('batchApi')).toBeTruthy()
+    expect(client.hasOwnProperty('searchApi')).toBeTruthy()
+    expect(client.hasOwnProperty('getAll')).toBeTruthy()
   });
 });
