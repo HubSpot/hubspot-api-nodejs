895d96621fe8e342ab5068564448a45be72509cc
ksvirkou <ksvirkou@hubspot.com>
2021-01-26 13:53:18 +0300

    added tets

---
+++ test/spec/apis.spec.ts
@@ -0,0 +1,12 @@
+import { Client } from '../../index'
+
+describe('api client', () => {
+  it('is discoverable', () => {
+    const client = new Client()
+    expect(client.hasOwnProperty('automation')).toBeTruthy()
+    expect(client.hasOwnProperty('crm')).toBeTruthy()
+    expect(client.hasOwnProperty('cms')).toBeTruthy()
+    expect(client.hasOwnProperty('oauth')).toBeTruthy()
+    expect(client.hasOwnProperty('webhooks')).toBeTruthy()
+  })
+})
