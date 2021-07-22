8f2ea70af54fa7d64bfd253213d3390918bdf170
ksvirkou <ksvirkou@hubspot.com>
2021-07-22 17:04:38 +0300

    add Files API + CommunicationPreferences API (Codegen only)

---
+++ codegen/files/api/apis.ts
@@ -0,0 +1,25 @@
+export * from './filesApi';
+import { FilesApi } from './filesApi';
+export * from './foldersApi';
+import { FoldersApi } from './foldersApi';
+import * as fs from 'fs';
+import * as http from 'http';
+
+export class HttpError extends Error {
+    constructor (public response: http.IncomingMessage, public body: any, public statusCode?: number) {
+        super('HTTP request failed');
+        this.name = 'HttpError';
+    }
+}
+
+export interface RequestDetailedFile {
+    value: Buffer;
+    options?: {
+        filename?: string;
+        contentType?: string;
+    }
+}
+
+export type RequestFile = string | Buffer | fs.ReadStream | RequestDetailedFile;
+
+export const APIS = [FilesApi, FoldersApi];
