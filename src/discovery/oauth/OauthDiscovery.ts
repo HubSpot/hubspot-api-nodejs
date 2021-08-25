d64076820934ccf93570c136940a9180f1a2dcd1
ksvirkou <ksvirkou@hubspot.com>
2021-08-25 12:29:30 +0300

    add get oauth url

--- src/discovery/oauth/OauthDiscovery.ts
+++ src/discovery/oauth/OauthDiscovery.ts
@@ -1,3 +1,5 @@
+import * as _ from 'lodash'
+import * as qs from 'querystring'
 import { createConfiguration } from '../../../codegen/oauth/configuration'
 import { AccessTokensApi, RefreshTokensApi, TokensApi } from '../../../codegen/oauth/index'
 import { IConfiguration } from '../../IConfiguration'
@@ -17,4 +19,23 @@ export class OauthDiscovery extends BaseDiscovery {
     this.refreshTokensApi = new RefreshTokensApi(configuration)
     this.tokensApi = new TokensApi(configuration)
   }
+
+  getAuthorizationUrl(
+    clientId: string,
+    redirectUri: string,
+    scope: string,
+    optionalScope?: string,
+    state?: string,
+  ): string {
+    const params = {
+      client_id: clientId,
+      redirect_uri: redirectUri,
+      scope,
+      optional_scope: optionalScope,
+      state,
+    }
+
+    return `https://app.hubspot.com/oauth/authorize?${qs.stringify(_.omitBy(params, _.isNil))}`
+  }
+
 }
