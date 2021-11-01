4fba822e3951702acd3cfa76fad3d9c99a1c7ed6
ksvirkou <ksvirkou@hubspot.com>
2021-11-01 17:58:46 +0300

    modify all classes

--- src/discovery/crm/deals/DealsDiscovery.ts
+++ src/discovery/crm/deals/DealsDiscovery.ts
@@ -1,9 +1,10 @@
+import * as _ from 'lodash'
 import { createConfiguration } from '../../../../codegen/crm/deals/configuration'
-import { AssociationsApi, BasicApi, BatchApi, SearchApi, SimplePublicObjectWithAssociations } from '../../../../codegen/crm/deals/index'
+import { AssociationsApi, BasicApi, BatchApi, CollectionResponseSimplePublicObjectWithAssociationsForwardPaging, SearchApi, SimplePublicObjectWithAssociations } from '../../../../codegen/crm/deals/index'
 import { IConfiguration } from '../../../IConfiguration'
-import { ObjectDiscovery } from '../ObjectDiscovery'
+import { BaseDiscovery } from '../../BaseDiscovery'
 
-export class DealsDiscovery extends ObjectDiscovery<SimplePublicObjectWithAssociations> {
+export class DealsDiscovery extends BaseDiscovery {
   public associationsApi: AssociationsApi
   public basicApi: BasicApi
   public batchApi: BatchApi
@@ -19,4 +20,17 @@ export class DealsDiscovery extends ObjectDiscovery<SimplePublicObjectWithAssoci
     this.batchApi = new BatchApi(configuration)
     this.searchApi = new SearchApi(configuration)
   }
+
+  public async getAll(properties?: Array<string>, associations?: Array<string>, archived?: boolean): Promise<SimplePublicObjectWithAssociations[]> {
+    let after
+    let result: SimplePublicObjectWithAssociations[] = []
+    do {
+      let response:CollectionResponseSimplePublicObjectWithAssociationsForwardPaging  = await this.basicApi.getPage(100, after, properties, associations, archived)
+      result = result.concat(response.results)
+      after = _.get(response, 'paging.next.after')
+    } while (!_.isNil(after))
+
+    return result
+  }
+  
 }
--- src/discovery/crm/quotes/QuotesDiscovery.ts
+++ src/discovery/crm/quotes/QuotesDiscovery.ts
@@ -1,9 +1,10 @@
+import * as _ from 'lodash'
 import { createConfiguration } from '../../../../codegen/crm/quotes/configuration'
-import { AssociationsApi, BasicApi, BatchApi, SearchApi, SimplePublicObjectWithAssociations } from '../../../../codegen/crm/quotes/index'
+import { AssociationsApi, BasicApi, BatchApi, CollectionResponseSimplePublicObjectWithAssociationsForwardPaging, SearchApi, SimplePublicObjectWithAssociations } from '../../../../codegen/crm/quotes/index'
 import { IConfiguration } from '../../../IConfiguration'
-import { ObjectDiscovery } from '../ObjectDiscovery'
+import { BaseDiscovery } from '../../BaseDiscovery'
 
-export class QuotesDiscovery extends ObjectDiscovery<SimplePublicObjectWithAssociations> {
+export class QuotesDiscovery extends BaseDiscovery {
   public associationsApi: AssociationsApi
   public basicApi: BasicApi
   public batchApi: BatchApi
@@ -19,4 +20,17 @@ export class QuotesDiscovery extends ObjectDiscovery<SimplePublicObjectWithAssoc
     this.batchApi = new BatchApi(configuration)
     this.searchApi = new SearchApi(configuration)
   }
+
+  public async getAll(properties?: Array<string>, associations?: Array<string>, archived?: boolean): Promise<SimplePublicObjectWithAssociations[]> {
+    let after
+    let result: SimplePublicObjectWithAssociations[] = []
+    do {
+      let response:CollectionResponseSimplePublicObjectWithAssociationsForwardPaging  = await this.basicApi.getPage(100, after, properties, associations, archived)
+      result = result.concat(response.results)
+      after = _.get(response, 'paging.next.after')
+    } while (!_.isNil(after))
+
+    return result
+  }
+
 }
