ec635ee8e38f94b3265847aac82be95d8d1da02b
ksvirkou <ksvirkou@hubspot.com>
2022-01-24 14:31:46 +0300

    cs fix

--- src/configuration/ApiClientConfirator.ts
+++ src/configuration/ApiClientConfirator.ts
@@ -4,45 +4,45 @@ import { IRequestContext } from '../services/IRequestContext'
 import { Observable } from '../services/Observable'
 
 export class ApiClientConfirator {
-    static getParams<RequestContextType extends IRequestContext, ResponseContextType>(config: IConfiguration) {
-        let params = {}
-        if (config.accessToken) {
-          params = {
-            authMethods: {
-              oauth2: {
-                accessToken: config.accessToken,
-              },
-              middleware: this.getHeaderMiddleware<RequestContextType, ResponseContextType>(),
-            },
-          }
-        } else if (config.apiKey) {
-          params = {
-            authMethods: {
-              hapikey: config.apiKey,
-            },
-            middleware: this.getHeaderMiddleware<RequestContextType, ResponseContextType>(),
-          }
-        } else if (config.developerApiKey) {
-          params = {
-            authMethods: {
-              hapikey: config.developerApiKey,
-            },
-            middleware: this.getHeaderMiddleware<RequestContextType, ResponseContextType>(),
-          }
-        }
-    
-        return params
+  public static getParams<RequestContextType extends IRequestContext, ResponseContextType>(config: IConfiguration) {
+    let params = {}
+    if (config.accessToken) {
+      params = {
+        authMethods: {
+          oauth2: {
+            accessToken: config.accessToken,
+          },
+          middleware: this.getHeaderMiddleware<RequestContextType, ResponseContextType>(),
+        },
+      }
+    } else if (config.apiKey) {
+      params = {
+        authMethods: {
+          hapikey: config.apiKey,
+        },
+        middleware: this.getHeaderMiddleware<RequestContextType, ResponseContextType>(),
+      }
+    } else if (config.developerApiKey) {
+      params = {
+        authMethods: {
+          hapikey: config.developerApiKey,
+        },
+        middleware: this.getHeaderMiddleware<RequestContextType, ResponseContextType>(),
+      }
     }
 
-    static getHeaderMiddleware<RequestContextType extends IRequestContext, ResponseContextType>() {
-        return {
-          pre(context: RequestContextType): Observable<RequestContextType> {
-            context.setHeaderParam('User-agent', `hubspot-api-client-nodejs; ${VERSION}`)
-            return new Observable<RequestContextType>(Promise.resolve(context))
-          },
-          post(context: ResponseContextType): Observable<ResponseContextType> {
-            return new Observable<ResponseContextType>(Promise.resolve(context))
-          },
-        }
+    return params
+  }
+
+  public static getHeaderMiddleware<RequestContextType extends IRequestContext, ResponseContextType>() {
+    return {
+      pre(context: RequestContextType): Observable<RequestContextType> {
+        context.setHeaderParam('User-agent', `hubspot-api-client-nodejs; ${VERSION}`)
+        return new Observable<RequestContextType>(Promise.resolve(context))
+      },
+      post(context: ResponseContextType): Observable<ResponseContextType> {
+        return new Observable<ResponseContextType>(Promise.resolve(context))
+      },
     }
+  }
 }
