9b7ca0ac9f943a58de4f2e4b6b8876247c92b4af
ksvirkou <ksvirkou@hubspot.com>
2022-07-11 18:15:13 +0300

    add getSignature

--- src/utils/signature.ts
+++ src/utils/signature.ts
@@ -4,40 +4,34 @@ import { ISignatureOptions } from './ISignatureOptions'
 
 export class Signature {
   public static isValid({ method = 'POST', signatureVersion = 'v1', ...options }: ISignatureOptions): boolean {
-    let sourceString = null
-    let hash = null
-
-    switch (signatureVersion) {
-      case 'v1':
-        sourceString = options.clientSecret + options.requestBody
-        hash = crypto
-          .createHash('sha256')
-          .update(sourceString)
-          .digest('hex')
-        break
-      case 'v2':
-        sourceString = options.clientSecret + method + options.url + options.requestBody
-        hash = crypto
-          .createHash('sha256')
-          .update(sourceString)
-          .digest('hex')
-        break
-      case 'v3':
-        sourceString = method + options.url + options.requestBody + options.timestamp
-        hash = crypto
-          .createHmac('sha256', options.clientSecret)
-          .update(sourceString)
-          .digest('base64')
-        break
-      default:
-        throw new Error(`Not supported signature version: ${signatureVersion}`)
-        break
-    }
-
+    let hash = Signature.getSignature(method, signatureVersion, options)
+    
     return _.isEqual(options.signature, hash)
   }
 
-  public getV3Signature(): string {
-    return 'f'
+  public static getSignature(method: string, signatureVersion: string, options: ISignatureOptions): string {
+    let sourceString = null
+    switch (signatureVersion) {
+      case 'v1':
+        sourceString = options.clientSecret + options.requestBody
+        return crypto
+          .createHash('sha256')
+          .update(sourceString)
+          .digest('hex')
+      case 'v2':
+        sourceString = options.clientSecret + method + options.url + options.requestBody
+        return crypto
+          .createHash('sha256')
+          .update(sourceString)
+          .digest('hex')
+      case 'v3':
+        sourceString = method + options.url + options.requestBody + options.timestamp
+        return crypto
+          .createHmac('sha256', options.clientSecret)
+          .update(sourceString)
+          .digest('base64')
+      default:
+        throw new Error(`Not supported signature version: ${signatureVersion}`)
+    }
   }
 }
