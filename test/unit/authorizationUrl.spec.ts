f2be87ed56d02528b4856129a5529bf1eac0ae17
ksvirkou-hubspot <ksvirkou@hubspot.com>
2022-12-05 14:53:51 +0300

    fix tests names

--- test/unit/authorizationUrl.spec.ts
+++ test/unit/authorizationUrl.spec.ts
@@ -1,7 +1,7 @@
 import { Client } from '../../index'
 
 describe('Check Authorization Url', () => {
-  it('Authorization Url with optional_scope', () => {
+  it('it Authorization Url with optional_scope', () => {
     const client = new Client()
     const url = client.oauth.getAuthorizationUrl('clientid', 'http://localhost', 'contacts timeline', 'scope1 scope2')
 
@@ -9,7 +9,7 @@ describe('Check Authorization Url', () => {
       'https://app.hubspot.com/oauth/authorize?client_id=clientid&redirect_uri=http%3A%2F%2Flocalhost&scope=contacts%20timeline&optional_scope=scope1%20scope2',
     ).toBe(url)
   })
-  it('Authorization Url without optional_scope', () => {
+  it('it Authorization Url without optional_scope', () => {
     const client = new Client()
     const url = client.oauth.getAuthorizationUrl('clientid', 'http://localhost', 'contacts timeline')
 
