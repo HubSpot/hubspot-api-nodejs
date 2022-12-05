7c266cb60dd8895d9457d680d12c178e7343d124
ksvirkou-hubspot <ksvirkou@hubspot.com>
2022-12-05 12:54:22 +0300

    add test for Authorization Url

---
+++ test/unit/authorizationUrl.spec.ts
@@ -0,0 +1,16 @@
+import { Client } from '../../index'
+
+describe('Check Authorization Url', () => {
+  it('Authorization Url with optional_scope', () => {
+    const client = new Client()
+    const url = client.oauth.getAuthorizationUrl('clientid', 'http://localhost', 'contacts timeline', 'scope1 scope2')
+
+    expect('https://app.hubspot.com/oauth/authorize?client_id=clientid&redirect_uri=http%3A%2F%2Flocalhost&scope=contacts%20timeline&optional_scope=scope1%20scope2').toBe(url)
+  })
+  it('Authorization Url without optional_scope', () => {
+    const client = new Client()
+    const url = client.oauth.getAuthorizationUrl('clientid', 'http://localhost', 'contacts timeline')
+
+    expect('https://app.hubspot.com/oauth/authorize?client_id=clientid&redirect_uri=http%3A%2F%2Flocalhost&scope=contacts%20timeline').toBe(url)
+  })
+})
