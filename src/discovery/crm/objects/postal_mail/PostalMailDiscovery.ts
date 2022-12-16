21d2a4e5103293387c71ed697626a930f7af946f
ksvirkou-hubspot <ksvirkou@hubspot.com>
2022-12-16 14:55:52 +0300

    add postal mail discovery

---
+++ src/discovery/crm/objects/postal_mail/PostalMailDiscovery.ts
@@ -0,0 +1,40 @@
+import { createConfiguration } from '../../../../../codegen/crm/objects/postal_mail/configuration'
+import {
+  AssociationsApi,
+  BasicApi,
+  BatchApi,
+  PublicObjectApi,
+  RequestContext,
+  ResponseContext,
+  SearchApi,
+  ServerConfiguration,
+} from '../../../../../codegen/crm/objects/postal_mail/index'
+import { Observable } from '../../../../../codegen/crm/objects/postal_mail/rxjsStub'
+import { ApiClientConfigurator } from '../../../../configuration/ApiClientConfigurator'
+import IConfiguration from '../../../../configuration/IConfiguration'
+
+export default class PostalMailDiscovery {
+  public associationsApi: AssociationsApi
+  public basicApi: BasicApi
+  public batchApi: BatchApi
+  public publicObjectApi: PublicObjectApi
+  public searchApi: SearchApi
+
+  constructor(config: IConfiguration) {
+    const configuration = createConfiguration(
+      ApiClientConfigurator.getParams<
+        RequestContext,
+        ResponseContext,
+        Observable<RequestContext>,
+        Observable<ResponseContext>,
+        ServerConfiguration<{}>
+      >(config, ServerConfiguration, Observable, Observable),
+    )
+
+    this.associationsApi = new AssociationsApi(configuration)
+    this.basicApi = new BasicApi(configuration)
+    this.batchApi = new BatchApi(configuration)
+    this.publicObjectApi = new PublicObjectApi(configuration)
+    this.searchApi = new SearchApi(configuration)
+  }
+}
---
+++ test/spec/crm/objects/postal_mail/postal_mail.spec.ts
@@ -0,0 +1,12 @@
+import { Client } from '../../../../../index'
+
+describe('api client', () => {
+  it('is discoverable', () => {
+    const client = new Client().crm.objects.postalMail
+    expect(client.hasOwnProperty('associationsApi')).toBeTruthy()
+    expect(client.hasOwnProperty('basicApi')).toBeTruthy()
+    expect(client.hasOwnProperty('batchApi')).toBeTruthy()
+    expect(client.hasOwnProperty('publicObjectApi')).toBeTruthy()
+    expect(client.hasOwnProperty('searchApi')).toBeTruthy()
+  })
+})
