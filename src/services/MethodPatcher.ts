64f481ce18f7f73a34d23cc8ee79366e41ab1cfe
ksvirkou-hubspot <ksvirkou@hubspot.com>
2023-01-05 13:16:11 +0300

    move constants to readonly properties

--- src/services/MethodPatcher.ts
+++ src/services/MethodPatcher.ts
@@ -1,16 +1,15 @@
 import get from 'lodash.get'
 import IConfiguration from '../configuration/IConfiguration'
 
-const RETRY_TIMEOUT = {
-  INTERNAL_SERVER_ERROR: 200,
-  TOO_MANY_REQUESTS: 10 * 1000,
-  TOO_MANY_SEARCH_REQUESTS: 1000,
-}
-
-const TEN_SECONDLY_ROLLING = 'TEN_SECONDLY_ROLLING'
-const SECONDLY_LIMIT_MESSAGE = 'You have reached your secondly limit.'
-
 export default class MethodPatcher {
+  public static readonly tenSecondlyRolling = 'TEN_SECONDLY_ROLLING'
+  public static readonly secondlyLimitMessage = 'You have reached your secondly limit.'
+  public static readonly retryTimeout = {
+    INTERNAL_SERVER_ERROR: 200,
+    TOO_MANY_REQUESTS: 10 * 1000,
+    TOO_MANY_SEARCH_REQUESTS: 1000,
+  }
+
   public static patch<Configuration>(config: IConfiguration, apiDiscovery: any, configuration: Configuration) {
     if (config.numberOfApiCallRetries !== undefined) {
       const properties = Object.getOwnPropertyNames(apiDiscovery)
@@ -60,21 +59,21 @@ export default class MethodPatcher {
           const statusCode: number = get(e, 'code', 0)
           console.error(statusCode)
           if (statusCode >= 500 && statusCode <= 599) {
-            await this._waitAfterRequestFailure(statusCode, index, RETRY_TIMEOUT.INTERNAL_SERVER_ERROR)
+            await this._waitAfterRequestFailure(statusCode, index, this.retryTimeout.INTERNAL_SERVER_ERROR)
             continue
           }
 
           if (statusCode === 429) {
             const policyName = get(e, 'body.policyName')
-            if (policyName === TEN_SECONDLY_ROLLING) {
-              await this._waitAfterRequestFailure(statusCode, index, RETRY_TIMEOUT.TOO_MANY_REQUESTS)
+            if (policyName === this.tenSecondlyRolling) {
+              await this._waitAfterRequestFailure(statusCode, index, this.retryTimeout.TOO_MANY_REQUESTS)
               continue
             }
 
             const message = get(e, 'body.message')
 
-            if (message === SECONDLY_LIMIT_MESSAGE) {
-              await this._waitAfterRequestFailure(statusCode, index, RETRY_TIMEOUT.TOO_MANY_SEARCH_REQUESTS)
+            if (message === this.secondlyLimitMessage) {
+              await this._waitAfterRequestFailure(statusCode, index, this.retryTimeout.TOO_MANY_SEARCH_REQUESTS)
               continue
             }
           }
