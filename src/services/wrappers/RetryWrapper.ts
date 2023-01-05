4d69656f965af164bb17c53ea714f2dfdd8d2747
ksvirkou-hubspot <ksvirkou@hubspot.com>
2023-01-05 18:09:54 +0300

    add status codes to readonly varible

--- src/services/wrappers/RetryWrapper.ts
+++ src/services/wrappers/RetryWrapper.ts
@@ -8,6 +8,11 @@ export default class RetryWrapper {
     TOO_MANY_REQUESTS: 10 * 1000,
     TOO_MANY_SEARCH_REQUESTS: 1000,
   }
+  public static readonly httpStatusCode = {
+    TOO_MANY_REQUESTS: 429,
+    MIN_SERVER_ERROR: 500,
+    MAX_SERVER_ERROR: 599,
+  }
 
   public static getWrappedMethod(method: any, numberOfApiCallRetries: number) {
     return async (...args: any) => {
@@ -29,12 +34,15 @@ export default class RetryWrapper {
 
           const statusCode: number = get(e, 'code', 0)
           console.error(statusCode)
-          if (statusCode >= 500 && statusCode <= 599) {
+          if (
+            statusCode >= this.httpStatusCode.MIN_SERVER_ERROR &&
+            statusCode <= this.httpStatusCode.MAX_SERVER_ERROR
+          ) {
             await this._waitAfterRequestFailure(statusCode, index, this.retryTimeout.INTERNAL_SERVER_ERROR)
             continue
           }
 
-          if (statusCode === 429) {
+          if (statusCode === this.httpStatusCode.TOO_MANY_REQUESTS) {
             const policyName = get(e, 'body.policyName')
             if (policyName === this.tenSecondlyRolling) {
               await this._waitAfterRequestFailure(statusCode, index, this.retryTimeout.TOO_MANY_REQUESTS)
