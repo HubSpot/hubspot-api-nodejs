8cf385cf0cb8d1dcc914c569aa5fb4baf46a49c3
ksvirkou-hubspot <ksvirkou@hubspot.com>
2024-01-18 14:35:19 +0300

    Update CRM discoveries

--- src/discovery/crm/quotes/QuotesDiscovery.ts
+++ src/discovery/crm/quotes/QuotesDiscovery.ts
@@ -3,6 +3,7 @@ import {
   BatchApi,
   Configuration,
   createConfiguration,
+  GDPRApi,
   PublicObjectApi,
   RequestContext,
   ResponseContext,
@@ -19,6 +20,7 @@ import { getAll } from '../../../services/getAll'
 export default class QuotesDiscovery {
   public basicApi: BasicApi
   public batchApi: BatchApi
+  public gdprApi: GDPRApi
   public publicObjectApi: PublicObjectApi
   public searchApi: SearchApi
 
@@ -35,6 +37,7 @@ export default class QuotesDiscovery {
 
     this.basicApi = ApiDecoratorService.getInstance().apply<BasicApi>(new BasicApi(configuration))
     this.batchApi = ApiDecoratorService.getInstance().apply<BatchApi>(new BatchApi(configuration))
+    this.gdprApi = ApiDecoratorService.getInstance().apply<GDPRApi>(new GDPRApi(configuration))
     this.publicObjectApi = ApiDecoratorService.getInstance().apply<PublicObjectApi>(new PublicObjectApi(configuration))
     this.searchApi = ApiDecoratorService.getInstance().apply<SearchApi>(new SearchApi(configuration))
   }
