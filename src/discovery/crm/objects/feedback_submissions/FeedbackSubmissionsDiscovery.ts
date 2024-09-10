89def7cd25f9492f740a2521eb8466beaff96c1a
ksvirkou-hubspot <ksvirkou@hubspot.com>
2024-09-10 18:17:37 +0300

    Update discoveries: CRM Calls, Emails, Feedback Submissions, Communications and Goals

--- src/discovery/crm/objects/feedback_submissions/FeedbackSubmissionsDiscovery.ts
+++ src/discovery/crm/objects/feedback_submissions/FeedbackSubmissionsDiscovery.ts
@@ -2,8 +2,6 @@ import { createConfiguration } from '../../../../../codegen/crm/objects/feedback
 import {
   BasicApi,
   BatchApi,
-  GDPRApi,
-  PublicObjectApi,
   RequestContext,
   ResponseContext,
   SearchApi,
@@ -17,8 +15,6 @@ import ApiDecoratorService from '../../../../services/ApiDecoratorService'
 export default class FeedbackSubmissionsDiscovery {
   public basicApi: BasicApi
   public batchApi: BatchApi
-  public gdprApi: GDPRApi
-  public publicObjectApi: PublicObjectApi
   public searchApi: SearchApi
 
   constructor(config: IConfiguration) {
