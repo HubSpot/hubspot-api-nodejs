4a1b8acf67e730b7ccf0dee9af57d009e03cd84e
Casey Grimes <casey@demandlab.com>
2024-12-02 17:21:04 -0500

    Documentation update and proposed test overhaul docs: Added verbose JSDoc markup to forms API files as proof of concept test: Included test/spec/marketing/forms/forms.spec.ts.proposed to demonstrate future enhancements

--- codegen/marketing/forms/apis/baseapi.ts
+++ codegen/marketing/forms/apis/baseapi.ts
@@ -1,8 +1,11 @@
 import { Configuration } from '../configuration'
 
 /**
+ * A collection of supported collection formats for API requests.
  *
  * @export
+ * @constant
+ * @type {{ csv: string; ssv: string; tsv: string; pipes: string }}
  */
 export const COLLECTION_FORMATS = {
     csv: ",",
@@ -11,26 +14,45 @@ export const COLLECTION_FORMATS = {
     pipes: "|",
 };
 
-
 /**
+ * A base class for API request factories that provides access to configuration settings.
  *
  * @export
- * @class BaseAPI
+ * @class BaseAPIRequestFactory
  */
 export class BaseAPIRequestFactory {
 
+    /**
+     * Creates an instance of BaseAPIRequestFactory.
+     *
+     * @param {Configuration} configuration - The configuration object for the API requests.
+     */
     constructor(protected configuration: Configuration) {
     }
-};
+}
 
 /**
+ * Represents an error thrown when a required parameter is missing during an API call.
  *
  * @export
  * @class RequiredError
  * @extends {Error}
  */
 export class RequiredError extends Error {
+    /**
+     * The name of the error.
+     *
+     * @type {"RequiredError"}
+     */
     name: "RequiredError" = "RequiredError";
+
+    /**
+     * Creates an instance of RequiredError.
+     *
+     * @param {string} api - The name of the API where the error occurred.
+     * @param {string} method - The name of the method where the error occurred.
+     * @param {string} field - The name of the missing field causing the error.
+     */
     constructor(public api: string, public method: string, public field: string) {
         super("Required parameter " + field + " was null or undefined when calling " + api + "." + method + ".");
     }
