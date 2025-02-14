2f81504081eb6ef981c3b7239d93f06b6b7616f3
ksvirkou-hubspot <ksvirkou@hubspot.com>
2025-02-14 15:49:15 +0300

    Lint: Add imports rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,47 @@
 import globals from "globals";
+import importPlugin from 'eslint-plugin-import';
 import eslint from '@eslint/js';
+import tsParser from '@typescript-eslint/parser';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  importPlugin.flatConfigs.recommended,
   {
     languageOptions: {
-      globals: globals.node
+      globals: globals.node,
+      parser: tsParser,
+      parserOptions: {
+        ecmaFeatures: {
+          modules: true
+        },
+        ecmaVersion: 'latest',
+        project: './tsconfig.json',
+      },
     },
+  },
+  {
+    settings: {
+      'import/resolver': {
+        typescript: true,
+        node: true
+      }
+    }
   },
   {
     ignores: ["codegen/", "lib/"]
   },
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
   {
     rules: {
       "@typescript-eslint/no-require-imports": "off",
-      "@typescript-eslint/no-explicit-any": "off"
+      "@typescript-eslint/no-explicit-any": "off",
+      "sort-imports": [
+        "error",
+        {
+          allowSeparatedGroups: false,
+        },
+      ]
     }
   }
 );
