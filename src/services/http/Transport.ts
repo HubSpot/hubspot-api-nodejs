5e748a940b1adcce758ed2a368c05406e1c8e9dd
ksvirkou-hubspot <ksvirkou@hubspot.com>
2026-06-29 16:31:51 +0300

    run linter

--- src/services/http/Transport.ts
+++ src/services/http/Transport.ts
@@ -197,11 +197,7 @@ function getHeadersFromNodeResponse(response: http.IncomingMessage): HeaderMap {
   return headers
 }
 
-function createBufferedResponse(
-  status: number,
-  headers: HeaderMap,
-  getBody: () => Promise<Buffer>,
-): IBufferedResponse {
+function createBufferedResponse(status: number, headers: HeaderMap, getBody: () => Promise<Buffer>): IBufferedResponse {
   let bodyPromise: Promise<Buffer> | undefined
 
   const readBody = async () => {
